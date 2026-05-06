f07493b5871d26e1f4a571669e71dcd73aa35124
Daniel <danielcampos.santos1603@gmail.com>
2026-05-06 16:01:03 -0300

    test: Atualização dos testes de aplicação
    - Atualizar teste de contexto/aplicação

---
+++ src/test/java/io/github/danielcampossantos/geralcar/GeralCarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielcampossantos.geralcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeralCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
