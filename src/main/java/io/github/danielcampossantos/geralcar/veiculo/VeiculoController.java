4abff7b61fc9affb8a61740c881230451039ecbe
Daniel <danielcampos.santos1603@gmail.com>
2026-05-06 16:01:03 -0300

    feat: Atualização do controller de veículo
    - Atualizar endpoints e comportamento do VeiculoController

---
+++ src/main/java/io/github/danielcampossantos/geralcar/veiculo/VeiculoController.java
@@ -0,0 +1,48 @@
+package io.github.danielcampossantos.geralcar.veiculo;
+
+import io.github.danielcampossantos.geralcar.veiculo.dto.VeiculoPostRequest;
+import io.github.danielcampossantos.geralcar.veiculo.dto.VeiculoGetResponse;
+import io.github.danielcampossantos.geralcar.veiculo.dto.VeiculoPostResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("veiculos")
+@RequiredArgsConstructor
+public class VeiculoController {
+    private final VeiculoService service;
+
+    @GetMapping
+    public ResponseEntity<Page<VeiculoGetResponse>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAllPage(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VeiculoGetResponse> findById(@PathVariable Long id) {
+        var response = service.findByIdOrThrowBadRequestException(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<VeiculoPostResponse> save(@Valid @RequestPart("data") VeiculoPostRequest request,
+                                                    @RequestPart("images") List<MultipartFile> images) {
+
+        var response = service.save(request, images);
+        return ResponseEntity.ok().body(response);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
