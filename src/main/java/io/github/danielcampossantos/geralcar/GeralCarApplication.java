34c0620165eb736e6624c9e857a2aee5dec59367
Daniel <danielcampos.santos1603@gmail.com>
2026-05-06 16:01:02 -0300

    feat: Configuração da inicialização e da camada web da aplicação
    - Atualizar classe principal GeralCarApplication
    
    - Ajustar configuração de segurança em SecurityConfig
    
    - Ajustar configuração MVC/cors em WebConfig
    
    - Incluir configurações de perfil e propriedades em application.yaml e application-dev.yaml

---
+++ src/main/java/io/github/danielcampossantos/geralcar/GeralCarApplication.java
@@ -0,0 +1,13 @@
+package io.github.danielcampossantos.geralcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeralCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeralCarApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/github/danielcampossantos/geralcar/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package io.github.danielcampossantos.geralcar.config;
+
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .build();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Arrays.asList("*"));     // qualquer origem
+        config.setAllowedMethods(Arrays.asList("*"));     // GET, POST, PUT, DELETE...
+        config.setAllowedHeaders(Arrays.asList("*"));     // qualquer header
+
+        // IMPORTANTE: com "*" isso deve ser false
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
